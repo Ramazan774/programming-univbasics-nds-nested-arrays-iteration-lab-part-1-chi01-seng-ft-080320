a030ea4c52e66ab17c06d0d9e362d5b925914f79
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-21 04:35:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,9 @@
 def find_even_values(src)
-  src = [
-    [10.11],
-    [99,50,3,4],
-    [23,41]
-    ]
-    
-    row_index = 0
+     row_index = 0
     while row_index< src.count do
       element_index = 0 
       while element_index < src [row_index].count do
-       if src[row_index][element_index].even?
+       if src[row_index][element_index].even? 
         p src[row_index][element_index]
       end
         element_index +=1  
